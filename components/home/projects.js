e70dd68579236fa2c0db900bb7ba2b87090bbc4c
RahulR100 <rahulrao@email.com>
2026-01-20 18:36:16 +0000

    updates to carousel

--- components/home/projects.js
+++ components/home/projects.js
@@ -6,46 +6,39 @@ import classes from './projects.module.css';
 const data = [
 	{
 		image: 'https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-		title: 'Best forests to visit in North America',
-		category: 'nature'
+		title: 'Spoke',
+		description: 'A brief description'
 	},
 	{
 		image: 'https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-		title: 'Hawaii beaches review: better than you think',
-		category: 'beach'
+		title: 'OptIndie News',
+		description: 'A brief description'
 	},
 	{
 		image: 'https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-		title: 'Mountains at night: 12 best locations to enjoy the view',
-		category: 'nature'
+		title: 'Envirotrack',
+		description: 'A brief description'
 	},
 	{
 		image: 'https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-		title: 'Aurora in Norway: when to visit for best experience',
-		category: 'nature'
+		title: 'Carpool Action',
+		description: 'A brief description'
 	},
 	{
 		image: 'https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-		title: 'Best places to visit this winter',
-		category: 'tourism'
-	},
-	{
-		image: 'https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80',
-		title: 'Active volcanos reviews: travel at your own risk',
-		category: 'nature'
+		title: 'National Voter File',
+		description: 'A brief description'
 	}
 ];
 
-function Card({ image, title, category }) {
+function Card({ image, title, description }) {
 	return (
 		<Paper shadow="md" p="xl" radius="md" style={{ backgroundImage: `url(${image})` }} className={classes.card}>
 			<div>
-				<Text className={classes.category} size="xs">
-					{category}
-				</Text>
-				<Title order={3} className={classes.title}>
+				<Title order={3} mt={0} mb={3} className={classes.title}>
 					{title}
 				</Title>
+				<Text className={classes.category}>{description}</Text>
 			</div>
 			<Button variant="white" color="dark">
 				Read article
@@ -76,7 +69,13 @@ export function Projects() {
 					Find More Resources
 				</Anchor>
 			</Group>
-			<Carousel slideSize={{ base: '100%', sm: '50%' }} slideGap={{ base: rem(2), sm: 'xl' }} align="start">
+			<Carousel
+				slideSize={{ base: '100%', sm: '50%' }}
+				withIndicators
+				withControls
+				slideGap={{ base: rem(2), sm: 'xl' }}
+				emblaOptions={{ loop: true, align: 'center' }}
+			>
 				{data.map((item, index) => (
 					<Carousel.Slide key={index}>
 						<Card {...item} />
